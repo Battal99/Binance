00b28b92f52f3c57695ca8fcd4eaf497f271e327
Battal Abdulaev <batal990@mail.ru>
2022-09-01 02:52:47 +0300

    fix main

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ time_data = []
 cost_data = []
 
 
-def update_graph():
+def update_graph() -> None:
     ax.plot(time_data, cost_data, color='b')
     ax.legend([f"Last price: {cost_data[-1]}$"])
 
@@ -37,26 +37,22 @@ def update_graph():
     plt.pause(0.1)
 
 
-async def write_in_file(time_data_list: list, cost_data_list: list):
+async def write_in_file(time_data_list: list, cost_data_list: list) -> None:
     with open('data.txt', 'w+') as file:
         file.write(f" Время {time_data_list} курс {cost_data_list}")
 
 
 async def main():
     async with websockets.connect(URL_BTC, ssl=ssl_context) as client:
-        try:
-            while True:
-                data = json.loads(await client.recv())['data']
-                event_time = time.localtime(data['E'] // 1000)
-                logging.info(data)
-                event_time = f"{event_time.tm_hour}:{event_time.tm_min}:{event_time.tm_sec}"
-                time_data.append(event_time)
-                cost_data.append(int(float(data['c'])))
-                update_graph()
-                await write_in_file(time_data, cost_data)
-
-        except KeyboardInterrupt:
-            logging.info("Скрипт остановлен")
+        while True:
+            data = json.loads(await client.recv())['data']
+            event_time = time.localtime(data['E'] // 1000)
+            logging.info(data)
+            event_time = f"{event_time.tm_hour}:{event_time.tm_min}:{event_time.tm_sec}"
+            time_data.append(event_time)
+            cost_data.append(int(float(data['c'])))
+            update_graph()
+            await write_in_file(time_data, cost_data)
 
 
 if __name__ == '__main__':
